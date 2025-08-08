e1e2e178c20540a58ecf0d957a2f2b6e9f1bf967
FatihNitro <FatihNitro@DESKTOP-KERR77G>
2025-08-09 01:06:38 +0300

    Add project files.

---
+++ Lists2_Pratik/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        List<string> kahveler = new List<string>();
+
+        for (int i = 1; i <= 5; i++)
+        {
+            Console.Write($"Kahve {i}: ");
+            string kahve = Console.ReadLine();
+            kahveler.Add(kahve);
+        }
+
+        Console.WriteLine("\nGirilen Kahve İsimleri:");
+        foreach (var kahve in kahveler)
+        {
+            Console.WriteLine(kahve);
+        }
+    }
+}
